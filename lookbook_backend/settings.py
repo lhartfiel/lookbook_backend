59da2dced282d686a9611d2ea4221bb2e7bbb432
Lindsay Hartfiel <lkhartfiel@uwalumni.com>
2025-10-27 14:55:59 -0600

    Update DRF permissions

--- lookbook_backend/settings.py
+++ lookbook_backend/settings.py
@@ -172,7 +172,7 @@ REST_FRAMEWORK = {
         "rest_framework.parsers.FormParser",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny"
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ],
 }
 
